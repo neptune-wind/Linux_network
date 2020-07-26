1df9c85959066fca408edf93f0ca194a7dee9d19
neptune-wind <316614796@qq.com>
2020-07-26 17:07:55 +0800

    Multi process concurrent server

--- 00_cs_model_v0.1/server.c
+++ 00_cs_model_v0.1/server.c
@@ -9,11 +9,12 @@
 #include <arpa/inet.h>
 #include "wrap.h"
 
-#define SERV_PORT   6666
-#define SERV_IP     "127.0.0.1"
+#define SERV_PORT	8888
+#define SERV_IP    	"127.0.0.1"
 
 int main(void)
 {
+    pid_t pid;
     int sfd, cfd;
     int len, i;
     char buf[BUFSIZ], clie_IP[BUFSIZ];
@@ -26,34 +27,51 @@ int main(void)
     bzero(&serv_addr, sizeof(serv_addr));
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(SERV_PORT);
+    inet_pton(AF_INET, SERV_IP, &serv_addr.sin_addr.s_addr);
 
     Bind(sfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
-    Listen(sfd, 2);
-
-    printf("wait for client connect ...\n");
-    clie_addr_len = sizeof(clie_addr);
-    cfd = Accept(sfd, (struct sockaddr*)&clie_addr, &clie_addr_len);
-    printf("cfd = ----%d\n", cfd);
-
-    printf("client IP: %s port: %d\n", 
-        inet_ntop(AF_INET, &clie_addr.sin_addr.s_addr, clie_IP, sizeof(clie_IP)),
-        ntohs(clie_addr.sin_port));
-
+    Listen(sfd, 3);
+    
     while(1)
     {
-        len = Read(cfd, buf, sizeof(buf));
-        Write(STDOUT_FILENO, buf, len);
-        for(i = 0; i < len; i++)
-        {
-            buf[i] = toupper(buf[i]);
-        }
-        Write(cfd, buf, len);
+	cfd = Accept(sfd, (struct sockaddr*)&clie_addr, &clie_addr_len);
+	
+	printf("client IP: %s port: %d\n", 
+	inet_ntop(AF_INET, &clie_addr.sin_addr.s_addr, clie_IP, sizeof(clie_IP)),
+	    ntohs(clie_addr.sin_port));
+	
+	pid = fork();
+	if(pid < 0)
+	{
+	    perror("fork error.");
+	    exit(1);
+	}
+	else if(pid == 0)	//子进程
+	{
+	    break;
+	}
+	else	//父进程
+	{
+	    close(cfd);
+	}
+    }
+    if(pid == 0)
+    {
+	while(1)
+	{
+	    close(sfd);
+	    len = Read(cfd, buf, sizeof(buf));
+	    for(i = 0; i < len; i++)
+	    {
+		buf[i] = toupper(buf[i]);
+	    }
+	    Write(cfd, buf, len);
+	}
+	close(cfd);
     }
 
-    Close(cfd);
     Close(sfd);
 
     return 0;
